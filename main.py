e7b00172d11d862914657d420582d6eb130d2ecc
llyyyy129-sudo <llyyyy129@gmail.com>
2026-05-19 16:17:36 +0800

    seed products in main

--- main.py
+++ main.py
@@ -11,6 +11,98 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 Base.metadata.create_all(bind=engine)
 
+def seed_products():
+
+    db = SessionLocal()
+
+    if db.query(Product).count() == 0:
+
+        products = [
+
+            Product(
+                id=1,
+                name="VR",
+                price=500,
+                description="Basic item",
+                image="/static/image/VR.jpg"
+            ),
+
+            Product(
+                id=2,
+                name="GP",
+                price=1000,
+                description="Premium item",
+                image="/static/image/GP.jpg"
+            ),
+
+            Product(
+                id=3,
+                name="SF",
+                price=520,
+                description="I LOVE YOU",
+                image="/static/image/SF.jpg"
+            ),
+
+            Product(
+                id=4,
+                name="PJ",
+                price=1314,
+                description="WILL BE TOGETHER FOREVER",
+                image="/static/image/PJ.jpg"
+            ),
+
+            Product(
+                id=5,
+                name="Heart",
+                price=9999,
+                description="LYY IS THE BEST",
+                image="/static/image/1.png"
+            ),
+            Product(
+                id=6, 
+                    name="BATHROOM_SET", 
+                    price=8888, 
+                    description="WXY IS THE BEST",
+                    image="/static/image/BATHROOM_SET.png"),
+            
+            Product(
+                    id=7, 
+                    name="COFFEE_MAKER", 
+                    price=6666, 
+                    description="WE ARE THE BEST",
+                    image="/static/image/COFFEE_MAKER.png"),
+
+            Product(
+                id=8, 
+                name="SNACK_BOX", 
+                price=4444, 
+                description="WE WILL BE TOGETHER FOREVER",
+                image="/static/image/SNACK_BOX.png"),
+
+
+            Product(
+                id=9, 
+                name="BAKE_SET", 
+                price=2222, 
+                description="I MISS YOU",
+                image="/static/image/BAKE_SET.png"),
+
+            Product(
+                id=10, 
+                name="STORGE_BOX", 
+                price=1111, 
+                description="I LOVE YOU TOO",
+                image="/static/image/STORGE_BOX.png")
+        ]
+
+        db.add_all(products)
+
+        db.commit()
+
+    db.close()
+
+seed_products()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
--- seed.py
+++ seed.py
@@ -17,11 +17,11 @@ def seed_products():
     Product(id=3, name="SF", price=520, description="I LOVE YOU",image="/static/image/SF.jpg"),
     Product(id=4, name="PJ", price=1314, description="WILL BE TOGETHER FOREVER",image="/static/image/PJ.jpg"),
     Product(id=5, name="Heart", price=9999, description="LYY IS THE BEST",image="/static/image/1.png"),
-    Product(id=6, name="BATHROOM SET", price=8888, description="WXY IS THE BEST",image="/static/image/BATHROOM_SET.png"),
-    Product(id=7, name="COFFEE MAKER", price=6666, description="WE ARE THE BEST",image="/static/image/COFFEE_MAKER.png"),
-    Product(id=8, name="SNACK BOX", price=4444, description="WE WILL BE TOGETHER FOREVER",image="/static/image/SNACK_BOX.png.png"),
-    Product(id=9, name="BAKE SET", price=2222, description="I MISS YOU",image="/static/image/BAKE_SET.png"),
-    Product(id=10, name="STORAGE BOX", price=1111, description="I LOVE YOU TOO",image="/static/image/STORGE_BOX.png"),
+    Product(id=6, name="BATHROOM_SET", price=8888, description="WXY IS THE BEST",image="/static/image/BATHROOM_SET.png"),
+    Product(id=7, name="COFFEE_MAKER", price=6666, description="WE ARE THE BEST",image="/static/image/COFFEE_MAKER.png"),
+    Product(id=8, name="SNACK_BOX", price=4444, description="WE WILL BE TOGETHER FOREVER",image="/static/image/SNACK_BOX.png"),
+    Product(id=9, name="BAKE_SET", price=2222, description="I MISS YOU",image="/static/image/BAKE_SET.png"),
+    Product(id=10, name="STORGE_BOX", price=1111, description="I LOVE YOU TOO",image="/static/image/STORGE_BOX.png")
    ]
         db.add_all(products)
         db.commit()
@@ -30,27 +30,5 @@ def seed_products():
 
 seed_products()
 
-products = [
-    Product(id=1, name="VR", price=500, description="Basic item",image="/static/image/VR.jpg"),
-    Product(id=2, name="GP", price=1000, description="Premium item",image="/static/image/GP.jpg"),
-    Product(id=3, name="SF", price=520, description="I LOVE YOU",image="/static/image/SF.jpg"),
-    Product(id=4, name="PJ", price=1314, description="WILL BE TOGETHER FOREVER",image="/static/image/PJ.jpg"),
-    Product(id=5, name="Heart", price=9999, description="LYY IS THE BEST",image="/static/image/1.png"),
-    Product(id=6, name="BATHROOM SET", price=8888, description="WXY IS THE BEST",image="/static/image/BATHROOM_SET.png"),
-    Product(id=7, name="COFFEE MAKER", price=6666, description="WE ARE THE BEST",image="/static/image/COFFEE_MAKER.png"),
-    Product(id=8, name="SNACK BOX", price=4444, description="WE WILL BE TOGETHER FOREVER",image="/static/image/SNACK_BOX.png.png"),
-    Product(id=9, name="BAKE SET", price=2222, description="I MISS YOU",image="/static/image/BAKE_SET.png"),
-    Product(id=10, name="STORAGE BOX", price=1111, description="I LOVE YOU TOO",image="/static/image/STORGE_BOX.png"),
-   ]
-
-for p in products:
-    existing = db.query(Product).filter(Product.id == p.id).first()
-
-    if not existing:
-        db.add(p)
-
-db.commit()
-
-db.close()
 
 print("Products inserted successfully")
