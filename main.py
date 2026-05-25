2f3d77fe653b27c68c79e5f6f4d40dd96cd12fc8
llyyyy129-sudo <llyyyy129@gmail.com>
2026-05-25 15:05:28 +0800

    Fix Depends import

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
