689c1a883a73b75fe5309c7cb48bbe359d729afa
llyyyy129-sudo <llyyyy129@gmail.com>
2026-06-02 17:28:34 +0800

    add reject order api

--- main.py
+++ main.py
@@ -569,3 +569,29 @@ def delete_order(order_id: int):
         "success": True,
         "message": f"Order {order_id} deleted"
     }
+
+@app.post("/sales/orders/{order_id}/reject")
+def reject_order(order_id: int):
+
+    db = SessionLocal()
+
+    order = db.query(Order).filter(
+        Order.id == order_id
+    ).first()
+
+    if not order:
+        db.close()
+        return {
+            "success": False,
+            "message": "Order not found"
+        }
+
+    order.status = "Rejected"
+
+    db.commit()
+    db.close()
+
+    return {
+        "success": True,
+        "message": "Order rejected successfully!"
+    }
